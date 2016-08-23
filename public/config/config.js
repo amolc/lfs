3c52e6638c3c19618e13addf1bd1adbd1d6cda2a
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-08-23 19:26:21 +0530

    untract files added

---
+++ public/config/config.js
@@ -0,0 +1,9 @@
+if (document.location.hostname == "node.fountaintechies.com") {
+
+    var baseUrl = "http://node.fountaintechies.com:11000/api/";
+  
+} else {
+
+    var baseUrl = "http://localhost:11000/api/";
+
+}
