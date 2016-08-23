7b9ada7ba8a2c94adead3138983526290a9a6ed9
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-08-23 19:24:36 +0530

    change in public folder

--- public1/config/config.js
+++
@@ -1,9 +0,0 @@
-if (document.location.hostname == "node.fountaintechies.com") {
-
-    var baseUrl = "http://node.fountaintechies.com:11000/api/";
-  
-} else {
-
-    var baseUrl = "http://localhost:11000/api/";
-
-}
