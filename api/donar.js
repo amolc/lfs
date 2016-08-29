3c2a8188279cf4b3ff638fd8e868ecce99f96eb0
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-08-29 18:07:00 +0530

    Admin search and UI changes on website

--- api/donar.js
+++ api/donar.js
@@ -182,27 +182,26 @@ router.get('/getalldonarlist', function(req, res) {
 });
 
 router.post('/searchdonor', function(req, res) {
-    //console.log("searchdonor");
-    //console.log("res 121:",req.body);
     var admin_id = req.body.id;
     var donorname = req.body.name;
     var donortype = req.body.type;
     var nominationcode = req.body.nomination;
 
     if (req.body.type) {
-        //console.log("search by type");
-        var sql = "SELECT * from donors where donortype LIKE '%" + donortype + "%'" + "AND admin_id = " + admin_id;
+        //var sql = "SELECT * from donors where donortype LIKE '%" + donortype + "%'" + "AND admin_id = " + admin_id;
+        var sql = "SELECT t1.*, t2.* FROM donors t1, donor_roles t2 WHERE t1.roleid = t2.roleid && t1.donortype LIKE '%"  + donortype + "%'" + "AND admin_id = " + admin_id;
     } else if (req.body.name) {
-        //console.log("search by name");
-        var sql = "SELECT * from donors where donorname LIKE '%" + donorname + "%'" + "AND admin_id = " + admin_id;
+        //var sql = "SELECT * from donors where donorname LIKE '%" + donorname + "%'" + "AND admin_id = " + admin_id;
+        var sql = "SELECT t1.*, t2.* FROM donors t1, donor_roles t2 WHERE t1.roleid = t2.roleid && t1.donorname LIKE '%"  + donorname + "%'" + "AND admin_id = " + admin_id;
+        //console.log("sql query:",sql);
     } else if (req.body.nomination) {
-        var sql = "SELECT * from donors where nominationcode LIKE '%" + nominationcode + "%'" + "AND admin_id = " + admin_id;
-        //console.log("search by nominationcode");
+        //var sql = "SELECT * from donors where nominationcode LIKE '%" + nominationcode + "%'" + "AND admin_id = " + admin_id;
+        var sql = "SELECT t1.*, t2.* FROM donors t1, donor_roles t2 WHERE t1.roleid = t2.roleid && t1.nominationcode LIKE '%"  + nominationcode + "%'" + "AND admin_id = " + admin_id;
     } else {
-        //console.log("search all");
-        var sql = "SELECT * from donors";
+        //var sql = "SELECT * from donors";
+        var sql = "SELECT t1.*, t2.* FROM donors t1, donor_roles t2 WHERE t1.roleid = t2.roleid"
     }
-    //console.log("sql query:",sql);
+
     connection.query(sql, function(error, response) {
         if (error) {
             console.log(error);
@@ -213,21 +212,16 @@ router.post('/searchdonor', function(req, res) {
 });
 
 router.post('/donorImport', function(req, res) {
-    //console.log("req body:",req.body);
     var csvData = req.body.csvData;
     var i = 0;
-    //console.log('req', csvData);
     var status = '';
     async.forEach(csvData, function(data, callback) {
-        // console.log("data:",data);
         if (data.donorname === undefined && data.roleid === undefined && data.donortype === undefined) {
             status = false;
             callback();
         } else {
             var query = "INSERT INTO donors (donorname,donortype,nominationcode,preftitle,roleid,admin_id,created_on,modified_on) VALUES ('" + data.donorname + "','" + data.donortype + "'," + data.nominationcode + ",'" + data.preftitle + "','" + data.roleid + "','" + req.body.id + "'," + env.timestamp() + "," + env.timestamp() + ")";
-            // console.log("query:",query);
             connection.query(query, function(err, dataL) {
-                //console.log('dataL',dataL);
                 if (!err) {
                     status = true;
                     i = ++i;
@@ -242,14 +236,13 @@ router.post('/donorImport', function(req, res) {
     }, function(error) {
         console.log('error async', error);
         if (!error) {
-            console.log("status:", status);
+            //console.log("status:", status);
             res.jsonp({ "status": status, count: i });
         }
     });
 });
 
 router.get('/getallFilterdonarlist', function(req, res) {
-    //console.log("getalldonarlist");
     var sql = 'SELECT donors.donorid,donors.donorname,donors.preftitle,donors.nominationcode,donors.donortype,donor_roles.role_name FROM donors INNER JOIN donor_roles ON donors.roleid=donor_roles.roleid';
     connection.query(sql, function(error, response) {
         if (error) {
@@ -262,9 +255,7 @@ router.get('/getallFilterdonarlist', function(req, res) {
 
 
 router.get('/donarlistbycorporate', function(req, res) {
-    //console.log("getalldonarlist");
-    var sql = "SELECT donors.donorid,donors.donorname,donors.preftitle,donors.nominationcode,donors.donortype,donor_roles.roleid,donor_roles.role_name,donor_roles.imageurl FROM donors INNER JOIN donor_roles ON donors.roleid=donor_roles.roleid WHERE donortype = " + "'Corporate Donors'";
-    //console.log("query:",sql);
+    var sql = "SELECT donors.donorid,donors.donorname,donors.preftitle,donors.nominationcode,donors.donortype,donor_roles.roleid,donor_roles.role_name,donor_roles.imageurl FROM donors INNER JOIN donor_roles ON donors.roleid=donor_roles.roleid WHERE donortype = 'Corporate Donors' ORDER BY donorid DESC";
     connection.query(sql, function(error, response) {
         if (error) {
             console.log(error);
@@ -275,9 +266,7 @@ router.get('/donarlistbycorporate', function(req, res) {
 });
 
 router.get('/donarlistbyIndividual', function(req, res) {
-    //console.log("getalldonarlist");
-    var sql = "SELECT donors.donorid,donors.donorname,donors.preftitle,donors.nominationcode,donors.donortype,donor_roles.roleid,donor_roles.role_name,donor_roles.imageurl FROM donors INNER JOIN donor_roles ON donors.roleid=donor_roles.roleid WHERE donortype = " + "'Individual Donors'";
-    //console.log("query:",sql);
+    var sql = "SELECT donors.donorid,donors.donorname,donors.preftitle,donors.nominationcode,donors.donortype,donor_roles.roleid,donor_roles.role_name,donor_roles.imageurl FROM donors INNER JOIN donor_roles ON donors.roleid=donor_roles.roleid WHERE donortype = 'Individual Donors' ORDER BY donorid DESC";
     connection.query(sql, function(error, response) {
         if (error) {
             console.log(error);
